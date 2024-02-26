7097ee26c3ea73bd9754e815e4afa92f23258495
Brenno Nascimento <brenno.nascimento@br.bosch.com>
2024-02-26 11:48:41 -0300

    build: remove default github config of next page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true
+  },
   basePath: '/no-server-nextjs-test',
   distDir: 'out'
 };
