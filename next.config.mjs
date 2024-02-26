9223bf47792d059cca82f0940cf84f129c8821a1
Brenno Nascimento <brenno.nascimento@br.bosch.com>
2024-02-26 12:04:25 -0300

    build: remove unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  },
   basePath: '/no-server-nextjs-test',
   distDir: 'out'
 };
